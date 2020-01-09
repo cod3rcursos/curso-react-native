b683ea726cbe62c1c8af68b165df6957dd29d57b
Wagner Costa <wcaquino@gmail.com>
2020-01-09 18:39:43 -0300

    TASKS-ONLINE-AULA21

--- tasks2020/src/common.js
+++ tasks2020/src/common.js
@@ -4,7 +4,11 @@ const server = Platform.OS === 'ios'
     ? 'http://localhost:3000' : 'http://10.0.2.2:3000'
 
 function showError(err) {
-    Alert.alert('Ops! Ocorreu um Problema!', `Mensagem: ${err}`)
+    if(err.response && err.response.data) {
+        Alert.alert('Ops! Ocorreu um Problema!', `Mensagem: ${err.response.data}`)
+    } else {
+        Alert.alert('Ops! Ocorreu um Problema!', `Mensagem: ${err}`)
+    }
 }
 
 function showSuccess(msg) {
