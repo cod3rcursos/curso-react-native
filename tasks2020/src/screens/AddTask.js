0da9c36f62c07b8ea79891d71844f1276cda0dde
Wagner Costa <wcaquino@gmail.com>
2020-01-08 21:54:25 -0300

    TASKS-OFFLINE-AULA18

--- tasks2020/src/screens/AddTask.js
+++ tasks2020/src/screens/AddTask.js
@@ -9,14 +9,22 @@ import {
     TouchableWithoutFeedback
 } from 'react-native'
 
+import DateTimePicker from '@react-native-community/datetimepicker'
+
 import commonStyles from '../commonStyles'
 
-const initialState = { desc: '' }
+const initialState = { desc: '', date: new Date() }
 
 export default class AddTask extends Component {
 
     state = {
         ...initialState
+    }
+
+    getDateTimePicker = () => {
+        return <DateTimePicker value={this.state.date}
+            onChange={(_, date) => this.setState({ date })}
+            mode='date' />
     }
 
     render() {
@@ -34,6 +42,7 @@ export default class AddTask extends Component {
                         placeholder="Informe a Descrição..."
                         onChangeText={desc => this.setState({ desc })}
                         value={this.state.desc} />
+                    {this.getDateTimePicker()}
                     <View style={styles.buttons}>
                         <TouchableOpacity onPress={this.props.onCancel}>
                             <Text style={styles.button}>Cancelar</Text>
