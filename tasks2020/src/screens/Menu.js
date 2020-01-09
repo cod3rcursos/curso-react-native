0bdbbcc5da80153a51318a17ecb8a9732fa1068b
Wagner Costa <wcaquino@gmail.com>
2020-01-09 18:36:36 -0300

    TASKS-ONLINE-AULA17

--- tasks2020/src/screens/Menu.js
+++ tasks2020/src/screens/Menu.js
@@ -2,19 +2,25 @@ import React from 'react'
 import { Platform, ScrollView, View, Text, StyleSheet } from 'react-native'
 import { DrawerItems } from 'react-navigation-drawer'
 import { Gravatar } from 'react-native-gravatar'
+import commonStyles from '../commonStyles'
 
 export default props => {
     return (
         <ScrollView>
             <View style={styles.header}>
+                <Text style={styles.title}>Tasks</Text>
                 <Gravatar style={styles.avatar} 
                     options={{
                         email: props.navigation.getParam('email'),
                         secure: true
                     }} />
                 <View style={styles.userInfo}>
-                    <Text>{props.navigation.getParam('name')}</Text>
-                    <Text>{props.navigation.getParam('email')}</Text>
+                    <Text style={styles.name}>
+                        {props.navigation.getParam('name')}
+                    </Text>
+                    <Text style={styles.email}>
+                        {props.navigation.getParam('email')}
+                    </Text>
                 </View>
             </View>
             <DrawerItems {...props} />
@@ -27,12 +33,34 @@ const styles = StyleSheet.create({
         borderBottomWidth: 1,
         borderColor: '#DDD'
     },
+    title: {
+        color: '#000',
+        fontFamily: commonStyles.fontFamily,
+        fontSize: 30,
+        paddingTop: Platform.OS === 'ios' ? 70 : 30,
+        padding: 10
+    },
     avatar: {
         width: 60,
         height: 60,
         borderWidth: 3,
         borderRadius: 30,
         margin: 10,
-        marginTop: Platform.OS === 'ios' ? 50 : 10
+        backgroundColor: '#222'
+    },
+    userInfo: {
+        marginLeft: 10,
+    },
+    name: {
+        fontFamily: commonStyles.fontFamily,
+        fontSize: 20,
+        color: commonStyles.colors.mainText,
+        marginBottom: 5,
+    },
+    email: {
+        fontFamily: commonStyles.fontFamily,
+        fontSize: 15,
+        color: commonStyles.colors.subText,
+        marginBottom: 10,
     }
 })
