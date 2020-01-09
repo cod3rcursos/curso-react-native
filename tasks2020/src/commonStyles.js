882552fcad25b3e708d5f3ee99a18dd593bb701e
Wagner Costa <wcaquino@gmail.com>
2020-01-08 21:45:06 -0300

    TASKS-OFFLINE-AULA15

--- tasks2020/src/commonStyles.js
+++ tasks2020/src/commonStyles.js
@@ -1,6 +1,7 @@
 export default {
     fontFamily: 'Lato',
     colors: {
+        today: '#B13B44',
         secondary: '#FFF',
         mainText: '#222',
         subText: '#555',
--- tasks2020/src/screens/AddTask.js
+++ tasks2020/src/screens/AddTask.js
@@ -2,9 +2,12 @@ import React, { Component } from 'react'
 import {
     Modal,
     View,
+    Text,
     StyleSheet,
     TouchableWithoutFeedback
 } from 'react-native'
+
+import commonStyles from '../commonStyles'
 
 export default class AddTask extends Component {
 
@@ -13,6 +16,13 @@ export default class AddTask extends Component {
             <Modal transparent={true} visible={this.props.isVisible}
                 onRequestClose={this.props.onCancel}
                 animationType='slide'>
+                <TouchableWithoutFeedback
+                    onPress={this.props.onCancel}>
+                    <View style={styles.background}></View>
+                </TouchableWithoutFeedback>
+                <View style={styles.container}>
+                    <Text style={styles.header}>Nova Tarefa</Text>
+                </View>
                 <TouchableWithoutFeedback
                     onPress={this.props.onCancel}>
                     <View style={styles.background}></View>
@@ -26,5 +36,17 @@ const styles = StyleSheet.create({
     background: {
         flex: 1,
         backgroundColor: 'rgba(0, 0, 0, 0.7)'
+    },
+    container: {
+        flex: 1,
+        backgroundColor: '#FFF'
+    },
+    header: {
+        fontFamily: commonStyles.fontFamily,
+        backgroundColor: commonStyles.colors.today,
+        color: commonStyles.colors.secondary,
+        textAlign: 'center',
+        padding: 15,
+        fontSize: 18
     }
 })
