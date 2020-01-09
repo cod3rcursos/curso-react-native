aadf14ff98a7cd1760fdbf28afd42baf5946edd2
Wagner Costa <wcaquino@gmail.com>
2020-01-08 21:37:40 -0300

    TASKS-OFFLINE-AULA03

--- tasks2020/src/screens/TaskList.js
+++ tasks2020/src/screens/TaskList.js
@@ -3,13 +3,20 @@ import { View, Text, ImageBackground, StyleSheet } from 'react-native'
 
 import todayImage from '../../assets/imgs/today.jpg'
 
+import moment from 'moment'
+import 'moment/locale/pt-br'
+
 export default class TaskList extends Component {
     render() {
+        const today = moment().locale('pt-br').format('ddd, D [de] MMMM')
         return (
             <View style={styles.container}>
                 <ImageBackground source={todayImage}
                     style={styles.background}>
-
+                    <View style={styles.titleBar}>
+                        <Text>Hoje</Text>
+                        <Text>{today}</Text>
+                    </View>
                 </ImageBackground>
                 <View style={styles.taskList}>
                     <Text>TaskList</Text>
@@ -28,5 +35,9 @@ const styles = StyleSheet.create({
     },
     taskList: {
         flex: 7
+    },
+    titleBar: {
+        flex: 1,
+        justifyContent: 'flex-end'
     }
 });
