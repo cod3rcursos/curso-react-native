5bc93a0f760cf39324b49fa467ef51e505d32a6b
Wagner Costa <wcaquino@gmail.com>
2020-01-09 18:31:28 -0300

    TASKS-ONLINE-AULA14

--- tasks2020/src/screens/TaskList.js
+++ tasks2020/src/screens/TaskList.js
@@ -16,9 +16,13 @@ import axios from 'axios'
 import moment from 'moment'
 import 'moment/locale/pt-br'
 
+import todayImage from '../../assets/imgs/today.jpg'
+import tomorrowImage from '../../assets/imgs/tomorrow.jpg'
+import weekImage from '../../assets/imgs/week.jpg'
+import monthImage from '../../assets/imgs/month.jpg'
+
 import { server, showError } from '../common'
 import commonStyles from '../commonStyles'
-import todayImage from '../../assets/imgs/today.jpg'
 import Task from '../components/Task'
 import AddTask from './AddTask'
 
@@ -111,6 +115,24 @@ export default class TaskList extends Component {
         }
     }
 
+    getImage = () => {
+        switch(this.props.daysAhead) {
+            case 0: return todayImage
+            case 1: return tomorrowImage
+            case 7: return weekImage
+            default: return monthImage
+        }
+    }
+
+    getColor = () => {
+        switch(this.props.daysAhead) {
+            case 0: return commonStyles.colors.today
+            case 1: return commonStyles.colors.tomorrow
+            case 7: return commonStyles.colors.week
+            default: return commonStyles.colors.month
+        }
+    }
+
     render() {
         const today = moment().locale('pt-br').format('ddd, D [de] MMMM')
         return (
@@ -118,7 +140,7 @@ export default class TaskList extends Component {
                 <AddTask isVisible={this.state.showAddTask}
                     onCancel={() => this.setState({ showAddTask: false })}
                     onSave={this.addTask} />
-                <ImageBackground source={todayImage}
+                <ImageBackground source={this.getImage()}
                     style={styles.background}>
                     <View style={styles.iconBar}>
                         <TouchableOpacity onPress={() => this.props.navigation.openDrawer()}>
@@ -140,7 +162,10 @@ export default class TaskList extends Component {
                         keyExtractor={item => `${item.id}`}
                         renderItem={({item}) => <Task {...item} onToggleTask={this.toggleTask} onDelete={this.deleteTask} />} />
                 </View>
-                <TouchableOpacity style={styles.addButton} 
+                <TouchableOpacity style={[
+                        styles.addButton,
+                        { backgroundColor: this.getColor() 
+                    }]} 
                     activeOpacity={0.7}
                     onPress={() => this.setState({ showAddTask: true })}>
                     <Icon name="plus" size={20}
@@ -192,7 +217,6 @@ const styles = StyleSheet.create({
         width: 50,
         height: 50,
         borderRadius: 25,
-        backgroundColor: commonStyles.colors.today,
         justifyContent: 'center',
         alignItems: 'center'
     }
