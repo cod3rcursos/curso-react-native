01ef3e242d096303f9838d17320de146554b58a0
Wagner Costa <wcaquino@gmail.com>
2020-01-08 21:33:48 -0300

    TASKS-OFFLINE-AULA01

---
+++ tasks2020/src/screens/TaskList.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import { View, Text } from 'react-native'
+
+export default class TaskList extends Component {
+    render() {
+        return (
+            <View>
+                <Text>TaskList</Text>
+            </View>
+        )
+    }
+}
