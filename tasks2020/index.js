afe6daa2b6e4d017731edd49e31d62752e65d928
Wagner Costa <wcaquino@gmail.com>
2020-01-09 18:13:08 -0300

    TASKS-ONLINE-AULA05

--- tasks2020/index.js
+++ tasks2020/index.js
@@ -3,7 +3,7 @@
  */
 
 import {AppRegistry} from 'react-native'
-import Auth from './src/screens/Auth'
+import Navigator from './src/Navigator'
 import {name as appName} from './app.json'
 
-AppRegistry.registerComponent(appName, () => Auth)
+AppRegistry.registerComponent(appName, () => Navigator)
---
+++ tasks2020/src/Navigator.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import { createAppContainer, createSwitchNavigator } from 'react-navigation'
+
+import Auth from './screens/Auth'
+import TaskList from './screens/TaskList'
+
+const mainRoutes = {
+    Auth: {
+        name: 'Auth',
+        screen: Auth
+    },
+    Home: {
+        name: 'Home',
+        screen: TaskList
+    }
+}
+
+const mainNavigator = createSwitchNavigator(mainRoutes, {
+    initialRouteName: 'Auth'
+})
+export default createAppContainer(mainNavigator)
