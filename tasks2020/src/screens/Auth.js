b3bf835a7ddeb1eb3a453d8a1bde47d136f50028
Wagner Costa <wcaquino@gmail.com>
2020-01-09 18:20:55 -0300

    TASKS-ONLINE-AULA08

--- tasks2020/src/screens/Auth.js
+++ tasks2020/src/screens/Auth.js
@@ -34,7 +34,7 @@ export default class Auth extends Component {
         if(this.state.stageNew) {
             this.signup()
         } else {
-            Alert.alert('Sucesso!', 'Logar')
+            this.signin()
         }
     }
 
@@ -49,6 +49,20 @@ export default class Auth extends Component {
 
             showSuccess('Usuário cadastro!')
             this.setState({ ...initialState })
+        } catch(e) {
+            showError(e)
+        }
+    }
+
+    signin = async () => {
+        try {
+            const res = await axios.post(`${server}/signin`, {
+                email: this.state.email,
+                password: this.state.password
+            })
+
+            axios.defaults.headers.common['Authorization'] = `bearer ${res.data.token}`
+            this.props.navigation.navigate('Home')
         } catch(e) {
             showError(e)
         }
