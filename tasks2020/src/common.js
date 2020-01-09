597e6d3b05a468f20ad8231c60764014da5dfa61
Wagner Costa <wcaquino@gmail.com>
2020-01-09 18:18:40 -0300

    TASKS-ONLINE-AULA07

---
+++ tasks2020/src/common.js
@@ -0,0 +1,14 @@
+import { Alert, Platform } from 'react-native'
+
+const server = Platform.OS === 'ios'
+    ? 'http://localhost:3000' : 'http://10.0.2.2:3000'
+
+function showError(err) {
+    Alert.alert('Ops! Ocorreu um Problema!', `Mensagem: ${err}`)
+}
+
+function showSuccess(msg) {
+    Alert.alert('Sucesso!', msg)
+}
+
+export { server, showError, showSuccess }
--- tasks2020/src/screens/Auth.js
+++ tasks2020/src/screens/Auth.js
@@ -8,25 +8,49 @@ import {
     Alert
 } from 'react-native'
 
+import axios from 'axios'
+
 import backgroundImage from '../../assets/imgs/login.jpg'
 import commonStyles from '../commonStyles'
 import AuthInput from '../components/AuthInput'
 
+import { server, showError, showSuccess } from '../common'
+
+const initialState = {
+    name: '',
+    email: '',
+    password: '',
+    confirmPassword: '',
+    stageNew: false
+}
+
 export default class Auth extends Component {
 
     state = {
-        name: '',
-        email: '',
-        password: '',
-        confirmPassword: '',
-        stageNew: false
+        ...initialState
     }
 
     signinOrSignup = () => {
         if(this.state.stageNew) {
-            Alert.alert('Sucesso!', 'Criar conta')
+            this.signup()
         } else {
             Alert.alert('Sucesso!', 'Logar')
+        }
+    }
+
+    signup = async () => {
+        try {
+            await axios.post(`${server}/signup`, {
+                name: this.state.name,
+                email: this.state.email,
+                password: this.state.password,
+                confirmPassword: this.state.confirmPassword,
+            })
+
+            showSuccess('Usuário cadastro!')
+            this.setState({ ...initialState })
+        } catch(e) {
+            showError(e)
         }
     }
     
