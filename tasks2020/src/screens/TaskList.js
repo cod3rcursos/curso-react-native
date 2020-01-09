4980b5e9e6dff07e663b4dbf6eded71712ceb082
Wagner Costa <wcaquino@gmail.com>
2020-01-08 21:35:11 -0300

    TASKS-OFFLINE-AULA02

--- tasks2020/src/screens/TaskList.js
+++ tasks2020/src/screens/TaskList.js
@@ -1,12 +1,32 @@
 import React, { Component } from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, ImageBackground, StyleSheet } from 'react-native'
+
+import todayImage from '../../assets/imgs/today.jpg'
 
 export default class TaskList extends Component {
     render() {
         return (
-            <View>
-                <Text>TaskList</Text>
+            <View style={styles.container}>
+                <ImageBackground source={todayImage}
+                    style={styles.background}>
+
+                </ImageBackground>
+                <View style={styles.taskList}>
+                    <Text>TaskList</Text>
+                </View>
             </View>
         )
     }
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1
+    },
+    background: {
+        flex: 3
+    },
+    taskList: {
+        flex: 7
+    }
+});
