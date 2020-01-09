8bdde712e8a5183a34bd5973cf980a82a6b6e8f4
Wagner Costa <wcaquino@gmail.com>
2020-01-08 21:40:03 -0300

    TASKS-OFFLINE-AULA07

--- tasks2020/src/components/Task.js
+++ tasks2020/src/components/Task.js
@@ -1,15 +1,36 @@
 import React from 'react'
 import { View, Text, StyleSheet } from 'react-native'
+import Icon from 'react-native-vector-icons/FontAwesome'
+
+import commonStyles from '../commonStyles'
 
 export default props => {
 
     return (
         <View style={styles.container}>
-            <Text>{props.desc}</Text>
-            <Text>{props.estimateAt + ""}</Text>
-            <Text>{props.doneAt + ""}</Text>
+            <View style={styles.checkContainer}>
+                {getCheckView(props.doneAt)}
+            </View>
+            <View>
+                <Text>{props.desc}</Text>
+                <Text>{props.estimateAt + ""}</Text>
+            </View>
         </View>
     )
+}
+
+function getCheckView(doneAt) {
+    if(doneAt != null) {
+        return (
+            <View style={styles.done}>
+                <Icon name='check' size={20} color='#FFF'></Icon>
+            </View>
+        )
+    } else {
+        return (
+            <View style={styles.pending}></View>
+        )
+    }
 }
 
 const styles = StyleSheet.create({
@@ -19,5 +40,25 @@ const styles = StyleSheet.create({
         borderBottomWidth: 1,
         alignItems: 'center',
         paddingVertical: 10
+    },
+    checkContainer: {
+        width: '20%',
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    pending: {
+        height: 25,
+        width: 25,
+        borderRadius: 13,
+        borderWidth: 1,
+        borderColor: '#555'
+    },
+    done: {
+        height: 25,
+        width: 25,
+        borderRadius: 13,
+        backgroundColor: '#4D7031',
+        alignItems: 'center',
+        justifyContent: 'center'
     }
 })
