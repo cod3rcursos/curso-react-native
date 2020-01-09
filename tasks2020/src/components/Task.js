14f7b4ca37c0cf706b31ffa27196ede202e1a493
Wagner Costa <wcaquino@gmail.com>
2020-01-08 21:39:19 -0300

    TASKS-OFFLINE-AULA06

--- tasks2020/src/components/Task.js
+++ tasks2020/src/components/Task.js
@@ -1,13 +1,23 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet } from 'react-native'
 
 export default props => {
 
     return (
-        <View>
+        <View style={styles.container}>
             <Text>{props.desc}</Text>
             <Text>{props.estimateAt + ""}</Text>
             <Text>{props.doneAt + ""}</Text>
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flexDirection: 'row',
+        borderColor: '#AAA',
+        borderBottomWidth: 1,
+        alignItems: 'center',
+        paddingVertical: 10
+    }
+})
