63fdd203cf19708a7ceb4793baa838de960cd2ab
Wagner Costa <wcaquino@gmail.com>
2020-01-09 18:09:03 -0300

    TASKS-ONLINE-AULA02

--- tasks2020/src/screens/Auth.js
+++ tasks2020/src/screens/Auth.js
@@ -1,16 +1,42 @@
 import React, { Component } from 'react'
-import { ImageBackground, Text, StyleSheet } from 'react-native'
+import {
+    ImageBackground,
+    Text,
+    StyleSheet,
+    View,
+    TextInput,
+    TouchableOpacity,
+    Platform
+} from 'react-native'
 
 import backgroundImage from '../../assets/imgs/login.jpg'
 import commonStyles from '../commonStyles'
 
 export default class Auth extends Component {
+
+    state = {
+        email: '',
+        password: ''
+    }
     
     render() {
         return (
             <ImageBackground source={backgroundImage}
                 style={styles.background}>
                 <Text style={styles.title}>Tasks</Text>
+                <View style={styles.formContainer}>
+                    <TextInput placeholder='E-mail' value={this.state.email}
+                        style={styles.input}
+                        onChangeText={email => this.setState({ email })} />
+                    <TextInput placeholder='Senha' value={this.state.password}
+                        style={styles.input}
+                        onChangeText={password => this.setState({ password })} />
+                    <TouchableOpacity>
+                        <View style={styles.button}>
+                            <Text style={styles.buttonText}>Entrar</Text>
+                        </View>
+                    </TouchableOpacity>
+                </View>
             </ImageBackground>
         )
     }
@@ -28,5 +54,26 @@ const styles = StyleSheet.create({
         color: commonStyles.colors.secondary,
         fontSize: 70,
         marginBottom: 10
+    },
+    formContainer: {
+        backgroundColor: 'rgba(0, 0, 0, 0.8)',
+        padding: 20,
+        width: '90%'
+    },
+    input: {
+        marginTop: 10,
+        backgroundColor: '#FFF',
+        padding: Platform.OS == 'ios' ? 15 : 10 
+    },
+    button: {
+        backgroundColor: '#080',
+        marginTop: 10,
+        padding: 10,
+        alignItems: 'center'
+    },
+    buttonText: {
+        fontFamily: commonStyles.fontFamily,
+        color: '#FFF',
+        fontSize: 20
     }
 })
