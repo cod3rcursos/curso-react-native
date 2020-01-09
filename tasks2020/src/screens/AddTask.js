3927d65e6c4871227e97f34f4b68164a8c32bd48
Wagner Costa <wcaquino@gmail.com>
2020-01-08 21:44:16 -0300

    TASKS-OFFLINE-AULA14

---
+++ tasks2020/src/screens/AddTask.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react'
+import {
+    Modal,
+    View,
+    StyleSheet,
+    TouchableWithoutFeedback
+} from 'react-native'
+
+export default class AddTask extends Component {
+
+    render() {
+        return (
+            <Modal transparent={true} visible={this.props.isVisible}
+                onRequestClose={this.props.onCancel}
+                animationType='slide'>
+                <TouchableWithoutFeedback
+                    onPress={this.props.onCancel}>
+                    <View style={styles.background}></View>
+                </TouchableWithoutFeedback>
+            </Modal>
+        )
+    }
+}
+
+const styles = StyleSheet.create({
+    background: {
+        flex: 1,
+        backgroundColor: 'rgba(0, 0, 0, 0.7)'
+    }
+})
--- tasks2020/src/screens/TaskList.js
+++ tasks2020/src/screens/TaskList.js
@@ -10,10 +10,12 @@ import moment from 'moment'
 import 'moment/locale/pt-br'
 
 import Task from '../components/Task'
+import AddTask from './AddTask'
 
 export default class TaskList extends Component {
     state = {
         showDoneTasks: true,
+        showAddTask: true,
         visibleTasks: [],
         tasks: [{
             id: Math.random(),
@@ -63,6 +65,8 @@ export default class TaskList extends Component {
         const today = moment().locale('pt-br').format('ddd, D [de] MMMM')
         return (
             <View style={styles.container}>
+                <AddTask isVisible={this.state.showAddTask}
+                    onCancel={() => this.setState({ showAddTask: false })} />
                 <ImageBackground source={todayImage}
                     style={styles.background}>
                     <View style={styles.iconBar}>
