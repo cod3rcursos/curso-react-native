70ba4928dca54470fc103aefb39aa2174a6fe43c
Wagner Aquino <wcaquino@gmail.com>
2022-01-29 10:12:02 -0600

    #016 - Posts via context

--- 2022/InstaClone/src/data/contexts/FeedContext.js
+++ 2022/InstaClone/src/data/contexts/FeedContext.js
@@ -3,7 +3,27 @@ import React, { createContext, useState } from "react"
 const FeedContext = createContext({})
 
 export const FeedProvider = ({ children }) => {
-    const [posts, setPosts] = useState([])
+    const [posts, setPosts] = useState([
+        {
+            id: Math.random(),
+            nickname: 'Rafael Pereira Filho',
+            email: 'rafaelprrflh@gmail.com',
+            image: require('../../../assets/imgs/fence.jpg'),
+            comments: [{
+                nickname: 'John Ray Sheldon',
+                comment: 'Stunning!'
+            }, {
+                nickname: 'Ana Julia Arruda',
+                comment: 'Foto linda! Onder foi tirada?'
+            }]
+        }, {
+            id: Math.random(),
+            nickname: 'Francisco Leandro Lima',
+            email: 'fllima@gmail.com',
+            image: require('../../../assets/imgs/bw.jpg'),
+            comments: []
+        }
+    ])
 
     const feedInternalContext = {
         posts
--- 2022/InstaClone/src/screens/Feed.js
+++ 2022/InstaClone/src/screens/Feed.js
@@ -3,35 +3,21 @@ import { FlatList, StyleSheet, View } from 'react-native'
 import Header from '../componentes/Header'
 import Post from '../componentes/Post'
 
-const posts = [{
-    id: Math.random(),
-    nickname: 'Rafael Pereira Filho',
-    email: 'rafaelprrflh@gmail.com',
-    image: require('../../assets/imgs/fence.jpg'),
-    comments: [{
-        nickname: 'John Ray Sheldon',
-        comment: 'Stunning!'
-    }, {
-        nickname: 'Ana Julia Arruda',
-        comment: 'Foto linda! Onder foi tirada?'
-    }]
-}, {
-    id: Math.random(),
-    nickname: 'Francisco Leandro Lima',
-    email: 'fllima@gmail.com',
-    image: require('../../assets/imgs/bw.jpg'),
-    comments: []
-}
-]
+import useFeed from '../data/hooks/useFeed'
 
-export default props => 
-    <View style={styles.container}>
-        <Header />
-        <FlatList
-            data={posts} keyExtractor={item => `${item.id}`}
-            renderItem={({item}) =>
-                <Post key={item.id} {...item} />} />
-    </View>
+export default props => {
+    const { posts } = useFeed()
+
+    return (
+        <View style={styles.container}>
+            <Header />
+            <FlatList
+                data={posts} keyExtractor={item => `${item.id}`}
+                renderItem={({item}) =>
+                    <Post key={item.id} {...item} />} />
+        </View>
+    )
+}
 
 const styles = StyleSheet.create({
     container: {
